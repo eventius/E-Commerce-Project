7bb1de2d49e21383f578367df3f3c803a7b1ea46
eventius <eventius.wennardi@gmail.com>
2024-04-18 16:10:06 +0700

    sidebar view page raw & produk page

---
+++ E-Commmerce-Project/Pages/Produk.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace E_Commmerce_Project.Pages
+{
+    public class ProdukModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
